1a6ce5d07b98376cdce029488067d5564ecb0801
Ahmad Samim <samim.dawlatzay@afghantelecom.af>
2026-01-05 14:16:40 +0430

    Create the Big o notation complixity file and create the example of o(1)

---
+++ week1/complexity.php
@@ -0,0 +1,20 @@
+<?php 
+// Example on O(1) Direct Array access
+$array = [1,2,3,4,5,6,7,8,9,10];
+$target = 9;
+
+$startTime = microtime(true);
+$result = $array[$target];
+echo $result;
+$endTime = microtime(true);
+$executionTime = $endTime - $startTime;
+echo 'Execution time: '.$executionTime.'micro seconds'. "\n";
+
+
+
+
+
+
+
+
+?>
