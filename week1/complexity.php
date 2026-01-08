c451e12ab8dfff637f2886e7ce111d43359f9ac0
Ahmad Samim <58666279+AhmadSamimDawlatzay@users.noreply.github.com>
2026-01-08 07:16:11 +0430

    Worked on complexity of on onn ologn

--- week1/complexity.php
+++ week1/complexity.php
@@ -20,7 +20,34 @@ $endTime = microtime(true);
 $executionTime = $endTime - $startTime;
 echo 'Execution time: '.$executionTime.'micro seconds'. "\n";
 
+// Example on O(n^2) Nested Loop
+$nestedArray = [[1,2,3],[4,5,6],[7,8,9]];
+$startTime = microtime(true);
+foreach($nestedArray as $ar){
+    foreach($ar as $innerAr){
+        echo $innerAr . "\n";
+    }
+}
+$endTime = microtime(true);
+$executionTime = $endTime - $startTime;
+echo 'Execution time: '.$executionTime.'micro seconds'. "\n";
 
+// Example on O(log n) Binary Search
+function binarySearch($array, $target) {
+    $left = 0;
+    $right = count($array) - 1;
+    while ($left <= $right) {
+        $mid = $left + floor(($right - $left) / 2);
+        if ($array[$mid] == $target) {
+            return $mid;
+        } elseif ($array[$mid] < $target) {
+            $left = $mid + 1;
+        } else {
+            $right = $mid - 1;
+        }
+    }
+    return -1;
+}
 
 
 ?>
